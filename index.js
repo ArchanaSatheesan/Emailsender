2749eff5ecaf7d4f95beab3bdce246447fb52509
Archana Satheesan <archana23892@gmail.com>
2023-01-12 20:11:31 +0530

    emailsender
    email sending using nodemailer

---
+++ index.js
@@ -0,0 +1,27 @@
+const nodemailer=require('nodemailer');
+const msg={
+    from:"archanasatheesantest@gmail.com",
+    to:"archana23892@gmail.com",
+    subject:"Test email assignment",
+    text:"testing first email via nodemailer package"
+};
+nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+        user:"archanasatheesantest@gmail.com",
+        pass:"cbvsytueswbqvkph"
+    },
+    port:465,
+    host:'smtp.gmail.com'
+
+})
+.sendMail(msg,(err)=>{
+    if(err){
+        return console.log("Error occured",err);
+    }
+    else{
+        return console.log("Email sent successfully");
+    }
+        
+	})
+	
